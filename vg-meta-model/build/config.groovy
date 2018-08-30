5d91e2dc2b9c103edba4e02099226475c58e657e
bale_lin <bale_lin@163.com>
2018-08-31 01:27:57 +0800

    测试Date, Double, Float, Integer, Long的Constraint

--- vg-meta-model/build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1')
-        classNode.putNodeMetaData('projectName', 'vg-meta-model')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
