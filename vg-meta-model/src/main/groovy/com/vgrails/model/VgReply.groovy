2a1dfedf4a844e901de657a72480422f6f493948
bale_lin <bale_lin@163.com>
2018-12-03 11:08:17 +0800

    init

--- vg-meta-model/src/main/groovy/com/vgrails/model/VgReply.groovy
+++
@@ -1,8 +0,0 @@
-package com.vgrails.model
-
-class VgReply {
-    int code = 0
-    String message = null
-
-    Map<String, Object> params = [:]
-}
--- vg-render-engine/grails-app/init/vg/render/engine/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package vg.render.engine
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
